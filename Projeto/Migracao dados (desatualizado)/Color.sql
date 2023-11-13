443e4ef176886023622d409f4c004787bea8477f
Tomás Gonçalves <83233093+tomasgoncalves02@users.noreply.github.com>
2023-11-13 16:56:18 +0000

    Delete Projeto directory

--- Projeto/Migracao dados (desatualizado)/Color.sql
+++
@@ -1,7 +0,0 @@
-create table Color(
-ColorKey INT IDENTITY(1,1) PRIMARY KEY  NOT NULL,
-Color nvarchar(55) NOT NULL);
-
-INSERT INTO Color (Color)
-SELECT Distinct Color
-FROM AdventureOldData.dbo.Products;
--- Projeto/Migracao dados (desatualizado)/Model.sql
+++
@@ -1,9 +0,0 @@
-create table Model (
-    ModelKey int IDENTITY(1,1) NOT NULL,
-    ModelName VARCHAR(255), 
-    PRIMARY KEY (modelKey)
-);
-
-INSERT INTO Model(ModelName)
-SELECT Distinct ModelName
-FROM AdventureOldData.dbo.Products;
