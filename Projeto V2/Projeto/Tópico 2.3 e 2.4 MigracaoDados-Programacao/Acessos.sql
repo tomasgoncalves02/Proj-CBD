12f3e23bbfd197f228ec316df1810bb57288a865
Tomás Gonçalves <83233093+tomasgoncalves02@users.noreply.github.com>
2023-11-17 12:57:11 +0000

    Delete Projeto V2 directory

--- Projeto V2/Projeto/Tópico 2.3 e 2.4 MigracaoDados-Programacao/Acessos.sql
+++
@@ -1,9 +0,0 @@
-﻿
--- Criação da tabela de Acessos
-CREATE TABLE Acessos (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    CustomerKey INT, 
-    DataAcesso DATETIME,
-    FOREIGN KEY (CustomerKey) REFERENCES Customer(CustomerKey)
-);
-
